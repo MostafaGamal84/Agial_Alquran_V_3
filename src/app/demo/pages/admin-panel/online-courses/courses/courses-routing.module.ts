05af1ab8e02347795081e7128ae15b588442e903
Mostafa Gamal <98846791+MostafaGamal84@users.noreply.github.com>
2025-09-04 10:37:21 +0300

    feat: add course detail view with student reports

--- src/app/demo/pages/admin-panel/online-courses/courses/courses-routing.module.ts
+++ src/app/demo/pages/admin-panel/online-courses/courses/courses-routing.module.ts
@@ -12,21 +12,34 @@ const routes: Routes = [
       {
         path: 'view',
         loadComponent: () => import('./courses-view/courses-view.component').then((c) => c.CoursesViewComponent),
-        data: { roles: [UserTypesEnum.Admin, UserTypesEnum.Manager,UserTypesEnum.BranchLeader,UserTypesEnum.Student,UserTypesEnum.Teacher] }
+        data: {
+          roles: [UserTypesEnum.Admin, UserTypesEnum.Manager, UserTypesEnum.BranchLeader, UserTypesEnum.Student, UserTypesEnum.Teacher]
+        }
+      },
+      {
+        path: 'details/:id',
+        loadComponent: () => import('./courses-details/courses-details.component').then((c) => c.CoursesDetailsComponent),
+        data: {
+          roles: [UserTypesEnum.Admin, UserTypesEnum.Manager, UserTypesEnum.BranchLeader, UserTypesEnum.Student, UserTypesEnum.Teacher]
+        }
       },
       {
         path: 'add',
         loadComponent: () => import('./courses-add/courses-add.component').then((c) => c.CoursesAddComponent),
-        data: { roles: [UserTypesEnum.Admin, UserTypesEnum.Manager,UserTypesEnum.BranchLeader,UserTypesEnum.Student,UserTypesEnum.Teacher] }
+        data: {
+          roles: [UserTypesEnum.Admin, UserTypesEnum.Manager, UserTypesEnum.BranchLeader, UserTypesEnum.Student, UserTypesEnum.Teacher]
+        }
       },
       {
         path: 'edit/:id',
         loadComponent: () => import('./courses-update/courses-update.component').then((c) => c.CoursesUpdateComponent),
-        data: { roles: [UserTypesEnum.Admin, UserTypesEnum.Manager,UserTypesEnum.BranchLeader,UserTypesEnum.Student,UserTypesEnum.Teacher] }
+        data: {
+          roles: [UserTypesEnum.Admin, UserTypesEnum.Manager, UserTypesEnum.BranchLeader, UserTypesEnum.Student, UserTypesEnum.Teacher]
+        }
       }
-      ]
-    }
-  ];
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
