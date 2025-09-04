6f2067b4f62812dca98d509e4ce08429c786551a
Mostafa Gamal <98846791+MostafaGamal84@users.noreply.github.com>
2025-09-04 10:45:51 +0300

    Pass circle via state to course details

---
+++ src/app/demo/pages/admin-panel/online-courses/courses/courses-details/courses-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { SharedModule } from 'src/app/demo/shared/shared.module';
+import { CircleDto, CircleStudentDto } from 'src/app/@theme/services/circle.service';
+
+@Component({
+  selector: 'app-courses-details',
+  imports: [SharedModule, RouterModule],
+  templateUrl: './courses-details.component.html',
+  styleUrl: './courses-details.component.scss'
+})
+export class CoursesDetailsComponent implements OnInit {
+  course?: CircleDto;
+  displayedColumns: string[] = ['fullName', 'action'];
+  dataSource = new MatTableDataSource<CircleStudentDto>();
+
+  ngOnInit() {
+    const course = history.state.course as CircleDto | undefined;
+    if (course) {
+      this.course = course;
+      this.dataSource.data = course.students || [];
+    }
+  }
+}
