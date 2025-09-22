eed0ec4f7c6dbdc103cf94ef124396e40290f9b9
MostafaGamal84 <mostafa.ddamoni22@gmail.com>
2025-09-22 12:47:43 +0300

    bv

--- src/app/demo/pages/admin-panel/online-courses/courses/courses-view/courses-view.component.ts
+++ src/app/demo/pages/admin-panel/online-courses/courses/courses-view/courses-view.component.ts
@@ -21,7 +21,7 @@ import {
 import { ToastService } from 'src/app/@theme/services/toast.service';
 import { AuthenticationService } from 'src/app/@theme/services/authentication.service';
 import { UserTypesEnum } from 'src/app/@theme/types/UserTypesEnum';
-import { formatDayValue } from 'src/app/@theme/types/DaysEnum';
+import { DAY_LABELS, formatDayValue } from 'src/app/@theme/types/DaysEnum';
 import { formatTimeValue } from 'src/app/@theme/utils/time';
 
 
