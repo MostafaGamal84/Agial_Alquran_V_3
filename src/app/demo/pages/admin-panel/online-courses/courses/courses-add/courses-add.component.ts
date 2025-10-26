785d437e0316237580804c3d884662559c031193
MostafaGamal84 <mostafa.ddamoni22@gmail.com>
2025-10-26 16:13:06 +0300

    SDF

--- src/app/demo/pages/admin-panel/online-courses/courses/courses-add/courses-add.component.ts
+++ src/app/demo/pages/admin-panel/online-courses/courses/courses-add/courses-add.component.ts
@@ -118,10 +118,6 @@ export class CoursesAddComponent implements OnInit, OnDestroy {
     this.destroy$.complete();
   }
 
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
 
   get daysArray(): FormArray<FormGroup> {
     return this.circleForm.get('days') as FormArray<FormGroup>;
