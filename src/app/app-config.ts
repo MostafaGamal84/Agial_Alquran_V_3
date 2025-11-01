c67f8c075d8f0f26eec2443bcb4c3eb216e5f697
MostafaGamal84 <mostafa.ddamoni22@gmail.com>
2025-11-01 12:47:04 +0300

    sdf

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,13 +1,13 @@
 export class AbleProConfig {
   static layout = 'vertical'; // vertical, horizontal, compact
-  static isDarkMode = 'light'; // light, dark, auto-mode
+  static isDarkMode = 'dark'; // light, dark, auto-mode
   static theme_color = 'blue-theme'; // blue-theme, indigo-theme, purple-theme, pink-theme, red-theme, orange-theme, yellow-theme, green-theme, teal-theme, cyan-theme
-  static isRtlLayout = false; // false true
+  static isRtlLayout = true; // false true
   static isBox_container = false; // false true
   static theme_contrast = false; //true false
   static menu_caption = false; // true false
   static isLanding = true;
-  static i18n = 'en'; // en, fr, ro, cn, ar
+  static i18n = 'ar'; // en, fr, ro, cn, ar
 }
 
 export const DASHBOARD_PATH = '/dashboard/default';
