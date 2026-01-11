fabfaa77df2c1345dc499bbc51cbb8c605d466d2
MostafaGamal84 <mostafa.ddamoni22@gmail.com>
2026-01-11 16:29:02 +0300

    شسي

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,7 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://ajyalbackend.somee.com'
+  // apiUrl: 'https://ajyalbackend.somee.com'
+    apiUrl: 'https://localhost:7260'
+  
 };
