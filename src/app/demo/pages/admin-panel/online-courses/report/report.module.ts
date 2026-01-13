e16a2506a33d2ba26810092a640d9f52a02ea3fb
MostafaGamal84 <mostafa.ddamoni22@gmail.com>
2026-01-13 13:10:02 +0300

    sdf

--- src/app/demo/pages/admin-panel/online-courses/report/report.module.ts
+++ src/app/demo/pages/admin-panel/online-courses/report/report.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 import { ReportRoutingModule } from './report-routing.module';
 
 @NgModule({
-  imports: [CommonModule, ReportRoutingModule]
+  imports: [CommonModule, MatPaginatorModule, MatTableModule, ReportRoutingModule]
 })
 export class ReportModule {}
